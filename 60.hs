258704343907166b98f6097770d9ab98cf0803aa
Tomasz Heimowski <tomasz.heimowski@outlook.com>
2016-03-13 08:45:45 +0100

    stack overflow :(

--- 60.hs
+++ 60.hs
@@ -1,17 +1,31 @@
-import Data.List
-import Data.Maybe
+--import Data.List
+--import Data.Maybe
 
 data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show,Eq)
 
---leaf a = Branch a Empty Empty
+--maxNodes h = floor $ 2 ** h - 1
 
-maxNodes h = floor $ 2 ** h - 1
-
+minNodes :: Integer -> Integer
 minNodes 0 = 0
 minNodes 1 = 1
 minNodes h = 1 + (minNodes (h - 1) + minNodes (h - 2))
 
-maxHeight n = fst $ fromJust $ find (\(h,ns) -> n >= ns) $ map (\h -> (h, minNodes h)) [0..]
+maxHeight :: Integer -> Integer
+maxHeight n =
+  fst $ 
+  last $ 
+  takeWhile (\(_,ns) -> n >= ns) $ 
+  map (\h -> (h, minNodes h)) [0..]
 
-main = print (map (maxHeight) [0..10])
---main = print (map maxHeight [0..10])
+hbalTreeNodes :: a -> Integer -> [Tree a]
+hbalTreeNodes _ 0 = [ Empty ]
+hbalTreeNodes x n =
+  [ Branch x l r | (a,b) <- [ (l,r) | l <- [0..n-1],
+                                      r <- [n-1-l],
+                                      minNodes (maxHeight l - 1) <= r,
+                                      minNodes (maxHeight r - 1) <= l ],
+                   l  <- hbalTreeNodes x a,
+                   r  <- hbalTreeNodes x b ]
+
+main :: IO ()
+main = print $ hbalTreeNodes 'x' 1
